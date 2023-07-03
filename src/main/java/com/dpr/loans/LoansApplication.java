768c054d488051bdb87c69623d710c0db16960b2
DeepaRajndr <133270634+DeepaRajndr@users.noreply.github.com>
2023-07-02 22:41:30 -0400

    Updated to get config properties from config server

--- src/main/java/com/dpr/loans/LoansApplication.java
+++ src/main/java/com/dpr/loans/LoansApplication.java
@@ -3,11 +3,13 @@ package com.dpr.loans;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScans;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@RefreshScope
 @ComponentScans({ @ComponentScan("com.dpr.loans.controller") })
 @EnableJpaRepositories("com.dpr.loans.repository")
 @EntityScan("com.dpr.loans.model")
--- src/main/java/com/dpr/loans/controller/LoansController.java
+++ src/main/java/com/dpr/loans/controller/LoansController.java
@@ -1,7 +1,7 @@
 package com.dpr.loans.controller;
 
 import java.util.List;
-
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,18 +9,21 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.dpr.loans.model.Customer;
 import com.dpr.loans.model.Loans;
+import com.dpr.loans.model.Properties;
 import com.dpr.loans.repository.LoansRepository;
-
-/**
- * @author Eazy Bytes
- *
- */
+import com.dpr.loans.config.LoansServiceConfig;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 
 @RestController
 public class LoansController {
 
     @Autowired
     private LoansRepository loansRepository;
+
+    @Autowired
+    LoansServiceConfig loansConfig;
 
     @PostMapping("/myLoans")
     public List<Loans> getLoansDetails(@RequestBody Customer customer) {
@@ -31,6 +34,15 @@ public class LoansController {
             return null;
         }
 
+    }
+
+    @GetMapping("/loans/properties")
+    public String getPropertyDetails() throws JsonProcessingException {
+        ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+        Properties properties = new Properties(loansConfig.getMsg(), loansConfig.getBuildVersion(),
+                loansConfig.getMailDetails(), loansConfig.getActiveBranches());
+        String jsonStr = ow.writeValueAsString(properties);
+        return jsonStr;
     }
 
 }
